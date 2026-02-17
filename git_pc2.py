1894c3668451ee1ee59cd5632f786a33dc1c60d1
Felipe_Mujica <147350719+PipeMujica@users.noreply.github.com>
2026-02-17 14:50:38 -0300

    Actualización desde Github

--- git_pc2.py
+++ git_pc2.py
@@ -1,1 +1,11 @@
 print("He creado un archivo desde el pc escritorio")
+#Para compartir el avance debo realizar los siguientes comandos:
+# git fetch -> git push -> git pull origin main (y se procede a actualizar en Github)
+print("Probando los cambios desde Github")
+
+#Para actualizar los cambios desde Github a Git (computadores, remoto) se deben usar los siguientes comandos:
+
+# git fetch origin (trae los cambios sin fusionarlos)
+# git merge origin/main = para fusionarlos manualmente 
+
+# git pull origin main (o master, segun el nombre indicado) = para fusionarlos automaticamente (esto hace el fetch (descarga los archivos) y merge (fusiona los archivos a la rama actual))
